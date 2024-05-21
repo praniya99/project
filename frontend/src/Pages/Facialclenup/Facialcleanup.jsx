7f47d61c3562901539445c8d18a07ad23d7b0ab7
E1124 <135853549+E1124@users.noreply.github.com>
2024-05-22 01:29:26 +0530

    facialcleanup

--- frontend/src/Pages/Facialclenup/Facialcleanup.jsx
+++ frontend/src/Pages/Facialclenup/Facialcleanup.jsx
@@ -6,15 +6,15 @@ const Facialcleanup = () => {
     <div>
       <Subheader/>
       <div className="big-rect12">
-            <div> <h2 className='heading-Nails'><b>Nails</b></h2></div>
+            <div> <h2 className='heading-facialcleanup'><b>facialcleanup</b></h2></div>
                 <div className="small-rect12">
                      <div >
                         <img src="../src/assets/T6.jpg"
                              alt="Description 1" 
-                             className="rect-image-Nails"/>
+                             className="rect-image-facialcleanup"/>
                         
                     </div>
-                    <div className="rect-heading-Nails">
+                    <div className="rect-heading-facialcleanup">
                     <h3><b>Let's make the most beautiful day of your life even more beautiful.</b></h3>
                     <h5 className="rect-head-Nails">A facial is a family of skin care treatments for the face, including steam, exfoliation, extraction, creams, lotions, facial masks, peels, and massage. They are normally performed in beauty salons, but are also a common spa treatment. They are used for general skin health as well as for specific skin conditions.</h5>
                     </div>
@@ -24,46 +24,46 @@ const Facialcleanup = () => {
                 <div className="small-rect12">
                     <div>
                     <ul>
-                        <b><h3 className="main-heading-Nails">Service</h3></b>
-                        <li className="li-Nails">Basic Manicure </li>
-                        <li className="li-Nails">Basic Pedicure</li>
-                        <li className="li-Nails">Fullset without colour</li>
-                        <li className="li-Nails">Fullset French</li>
-                        <li className="li-Nails">Refill</li>
-                        <li className="li-Nails">Buff & Remove Gel Extentions</li>
+                        <b><h3 className="main-heading-facialcleanup">Service</h3></b>
+                        <li className="li-facialcleanup">Basic Manicure </li>
+                        <li className="li-facialcleanup">Basic Pedicure</li>
+                        <li className="li-facialcleanup">Basic Manicure </li>
+                        <li className="li-facialcleanup">Basic Pedicure</li>
+                        <li className="li-facialcleanup">Basic Manicure </li>
+                        <li className="li-facialcleanup">Basic Pedicure</li>
                        
                     </ul>
                     </div>
-                    <div className='list-Nails'>
+                    <div className='list-facialcleanups'>
 
                    
 
                     <ul type='none'>
-                    <b><h3 className="main-heading-Nails1">Time Duration</h3></b>
-                    <li className="li-Nails">30 min </li>
-                        <li className="li-Nails">30 min </li>
-                        <li className="li-Nails">30 min </li>
-                        <li className="li-Nails">30 min </li>
-                        <li className="li-Nails">30 min </li>
-                        <li className="li-Nails">30 min </li>
+                    <b><h3 className="main-heading-facialcleanup1">Time Duration</h3></b>
+                    <li className="li-facialcleanup">30 min </li>
+                        <li className="li-facialcleanup">30 min </li>
+                        <li className="li-facialcleanup">30 min </li>
+                        <li className="li-facialcleanup">30 min </li>
+                        <li className="li-facialcleanup">30 min </li>
+                        <li className="li-facialcleanup">30 min </li>
                         
                         
                     </ul>
                     </div>
 
-                    <div className='list-Nails'>
+                    <div className='list-facialcleanup'>
 
                    
 
                     <ul type='none'>
-                    <b><h3 className="main-heading-Nails">Price</h3></b>
+                    <b><h3 className="main-heading-facialcleanup">Price</h3></b>
                         
-                        <li className="li-Nails">Rs.2300.00 </li>
-                        <li className="li-Nails">Rs.2900.00 </li>
-                        <li className="li-Nails">Rs.600.00 </li>
-                        <li className="li-Nails">Rs.6500.00 </li>
-                        <li className="li-Nails">Rs.4500.00 </li>
-                        <li className="li-Nails">Rs.1700.00 </li>
+                        <li className="li-facialcleanup">Rs.2300.00 </li>
+                        <li className="li-facialcleanup">Rs.2900.00 </li>
+                        <li className="li-facialcleanup">Rs.600.00 </li>
+                        <li className="li-facialcleanup">Rs.6500.00 </li>
+                        <li className="li-facialcleanup">Rs.4500.00 </li>
+                        <li className="li-facialcleanup">Rs.1700.00 </li>
                         
                         
                     </ul>
