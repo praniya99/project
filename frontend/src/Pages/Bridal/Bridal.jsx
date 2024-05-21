a01ad4a04b4a197a19619d056bdb3478167e5ba4
E1124 <135853549+E1124@users.noreply.github.com>
2024-05-22 01:05:32 +0530

    Bridalpage

---
+++ frontend/src/Pages/Bridal/Bridal.jsx
@@ -0,0 +1,73 @@
+import Subheader from "../../components/Subheader/Subheader"
+import './Bridal.css'
+const bridal = () => {
+  return (
+    <div>
+      <Subheader/>
+        <div className="big-rect1">
+        <div> <h2 className='heading-Bride'><b>Bridal</b></h2></div>
+            <div className="small-rect1">
+                <div >
+                        
+                            <img src="../src/assets/T3.jpg"
+                             alt="Description 1" 
+                             className="rect-image-Bride"/>
+                        
+                </div>
+                <div className="rect-heading-Bride">
+                    <h3><b>Let's make the most beautiful day of your life even more beautiful.</b></h3>
+                    <h5 className="rect-head-Bride">Bridal services encompass a range of offerings to prepare a bride for her wedding day. 
+                    This includes traditional hair and makeup styling, intricate henna designs, 
+                    dressing in traditional attire, and sometimes jewelry rental or styling. 
+                    Bridal makeup blends traditional and modern techniques, enhancing natural features while adhering to cultural norms. 
+                    Hairstyles often feature elaborate designs with flowers or traditional ornaments. Henna application is significant, 
+                    symbolizing beauty and auspiciousness. Overall, these services celebrate Sri Lankan cultural heritage while catering to individual preferences.</h5>
+                </div>
+            </div>
+
+
+            <div className="small-rect1">
+            <div>
+<ul>
+<b><h3 className="main-heading-Bride">Service</h3></b>
+    <li className="li-Bride">saree Draping </li>
+    <li className="li-Bride">Hair style</li>
+    <li className="li-Bride">Bridal Makeup</li>
+    <li className="li-Bride">Complete Bridal dressing Package(include hair,makeup and sari draping)</li>
+    <li className="li-Bride">Early Morning Dressing Charges
+     For services between 06.00 am and 9.00 am, 
+     an additional charge of Rs4,130.00 applies.</li>
+</ul>
+</div>
+                    <div className='list-Bride'><ul type='none'>
+                    <b><h3 className="main-heading-Bride">Price</h3></b>
+                        <li className="li-Bride">Rs.100.00 </li>
+                        <li className="li-Bride">Rs.100.00 </li>
+                        <li className="li-Bride">Rs.100.00 </li>
+                        <li className="li-Bride">Rs.100.00 </li>
+                        <li className="li-Bride">Rs.100.00 </li>
+                        <li className="li-Bride">Rs.100.00 </li>
+                        <li className="li-Bride">Rs.100.00 </li>
+                        <li className="li-Bride">Rs.100.00 </li>
+                    </ul></div>
+
+                    <div className='list-Bride'><ul type='none'>
+                    <b><h3 className="main-heading-Bride1">Time Duration</h3></b>
+                        <li className="li-Bride">15 min </li>
+                        <li className="li-Bride">15 min </li>
+                        <li className="li-Bride">15 min </li>
+                        <li className="li-Bride">15 min </li>
+                        <li className="li-Bride">15 min </li>
+                        <li className="li-Bride">15 min </li>
+                    </ul></div>
+
+                    
+                    </div>
+        </div>
+    </div>
+     
+    
+  )
+}
+
+export default bridal
