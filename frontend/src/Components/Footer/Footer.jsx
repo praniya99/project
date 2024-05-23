eaa2eb2e03d74a943b3201fab44c94069cd99d3d
E1124 <135853549+E1124@users.noreply.github.com>
2024-05-23 07:30:45 +0530

    Changes

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -3,9 +3,10 @@ import './Footer.css';
 
 const Footer = () => {
   return (
-    <div className="contact-container">
-    <div className="contact-info">
-      <h2>Contact US</h2>
+   <div className='big-rect10'>
+    <div className='small-rect10'>
+      <div className='small-rect10-1'><div className="contact-info">
+      <h2><b>Contact US</b></h2>
       <ul>
         <li><i className="fas fa-map-marker-alt"></i> Address line</li>
         <li><i className="fas fa-phone"></i> Phone no</li>
@@ -18,8 +19,10 @@ const Footer = () => {
         <i className="fab fa-instagram"></i>
       </div>
     </div>
-    <div className="contact-form">
-      <h2>Leave a comment</h2>
+</div>
+      <div className='small-rect10-2'>
+      <div className="contact-form">
+      <h2><b>Leave a comment</b></h2>
       <form>
         <div className="form-group">
           <input type="text" placeholder="Name" required />
@@ -32,8 +35,11 @@ const Footer = () => {
           <button type="button">Reviews</button>
         </div>
       </form>
-    </div>
-  </div>
+    </div>
+</div></div>
+    
+  
+   </div>
 );
 };
   
