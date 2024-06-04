5d9a66233bd37a2f56c7ec572f84b27631ca9a3c
Maheesha0720 <maheeshamaduwanthi2002@gmail.com>
2024-06-04 22:22:51 +0530

    changes_01

---
+++ frontend/src/components/PlusMark/Plus.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './plus.css'
+
+export default function Plus() {
+  return (
+    <div className='btn'>
+ <button className='btn01'>
+      +
+      </button>
+      <p className='para'>Add Cashier assistant</p>
+     
+    </div>
+  )
+}
---
+++ frontend/src/pages/Salary/Salary.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import '../Salary/salary.css'
+
+import Table from 'react-bootstrap/Table';
+
+
+export default function Salary() {
+  return (
+    <div>
+      <div className='topicBox'>
+        <p>Salary</p>
+     </div>
+     <div >
+     <Table striped bordered hover className="rounded-table">
+  <thead>
+    <tr className='tr'>
+      <th> Name</th>
+      <th>Month</th>
+      <th>Basic(Rs.)</th>
+      <th>Bonas(Rs.)</th>
+      <th>Leaves</th>
+      <th>Reduction</th>
+      <th></th>
+      <th>Salary(Rs.)</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr className='tr'>
+      <td>W.H.Nimali</td>
+      <td>January</td>
+      <td>60 000</td>
+      <td>30 000</td>
+      <td>-</td>
+      <td>-</td>
+      <td></td>
+      <td>90 000</td>
+
+
+    </tr>
+    <tr className='tr'>
+    <td>W.H.Nimali</td>
+      <td>February</td>
+      <td>60 000</td>
+      <td>20 000</td>
+      <td>-</td>
+      <td>-</td>
+      <td></td>
+      <td>80 000</td>
+
+    </tr>
+  </tbody>
+</Table>
+
+     </div>
+    </div>
+  )
+}
