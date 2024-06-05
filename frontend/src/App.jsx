34d56a7224b351e9e612b54195c6575363987ba5
HiruniNisansala <hiruninisansala595@gmail.com>
2024-06-05 11:08:31 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,32 @@
 
-import './App.css'
-import React from 'react'
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Home from './homePage/home';
-import Cart from './cartPage/cart';
-import AllProduct from './page/allProduct';
-import AddItem from './addItemPage/addItem';
-import SkinCare from './page/skinCare';
-import MakeUp from './page/makeUp';
-import HairCare from './page/hairCare';
-import BodyCare from './page/bodyCare';
-import Item from './page/item'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './homePage/Home';
+import Cart from './cartPage/Cart';
+import AllProduct from './page/AllProduct';
+import AddItem from './addItemPage/AddItem';
+import SkinCare from './page/SkinCare';
+import MakeUp from './page/MakeUp';
+import HairCare from './page/HairCare';
+import BodyCare from './page/BodyCare';
+import Item from './page/Item';
 
 function App() {
-  
- 
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-         <Route path="/cart" element={<Cart />} />
-         <Route path="/addItem" element={<AddItem />} />
-         <Route path="/allProduct" element={<AllProduct />} />
-         <Route path="/bodyCare" element={<BodyCare />} />
-         <Route path="/hairCare" element={<HairCare/>} />
-         <Route path="/skinCare" element={<SkinCare/>} />
-         <Route path="/makeUp" element={<MakeUp/>} />
-         <Route path="/item" element={<Item/>} />
-       
-
-
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/addItem" element={<AddItem />} />
+        <Route path="/allProduct" element={<AllProduct />} />
+        <Route path="/bodyCare" element={<BodyCare />} />
+        <Route path="/hairCare" element={<HairCare />} />
+        <Route path="/skinCare" element={<SkinCare />} />
+        <Route path="/makeUp" element={<MakeUp />} />
+        <Route path="/item" element={<Item />} />
       </Routes>
     </Router>
   );
 }
 
-export default App
+export default App;
